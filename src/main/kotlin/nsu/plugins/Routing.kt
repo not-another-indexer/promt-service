3acec932c7943513ceec0e37ed9eca80c66f8a56
Даниил Давыдов <davydovdaniil501@gmail.com>
2024-10-09 22:21:04 +0700

    feat(api): add Swagger-UI support for image handling

--- src/main/kotlin/nsu/plugins/Routing.kt
+++ src/main/kotlin/nsu/plugins/Routing.kt
@@ -1,6 +1,8 @@
 package nsu.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -8,6 +10,24 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        swaggerUI(path = "api/gallery", swaggerFile = "api/v1/gallery.yaml")
+
+        post("/search-images") {
+            call.respond(HttpStatusCode.NotImplemented, "Not Implemented")
+        }
+
+        post("/delete-image") {
+            call.respond(HttpStatusCode.NotImplemented, "Not Implemented")
+        }
+
+        post("/add-image") {
+            call.respond(HttpStatusCode.NotImplemented, "Not Implemented")
+        }
+
+        get("/get-images") {
+            call.respond(HttpStatusCode.NotImplemented, "Not Implemented")
         }
     }
 }
