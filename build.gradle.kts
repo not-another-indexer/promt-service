d8296134a3a9d0a51438b5179b69fff72b7de628
Eric Shelbogashev <96541869+EricShelbogashev@users.noreply.github.com>
2024-10-12 22:09:27 +0700

    added client

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+import com.google.protobuf.gradle.id
+
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    application
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "1.4.21"
     id("io.ktor.plugin") version "3.0.0-rc-2"
+    id("io.ktor.plugin") version "3.0.0-rc-1"
+    id("com.google.protobuf") version "0.9.2"
 }
 
 group = "nsu"
@@ -23,14 +26,22 @@ repositories {
 }
 
 dependencies {
+    // ktor
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-serialization-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
     implementation("io.ktor:ktor-server-swagger")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
+    // logback
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    // protobuf
+    implementation("io.grpc:grpc-kotlin-stub:1.2.0")
+    implementation("io.grpc:grpc-netty-shaded:1.53.0")
+    implementation("io.grpc:grpc-protobuf:1.53.0")
+    implementation("io.grpc:grpc-stub:1.53.0")
+    implementation("com.google.protobuf:protobuf-java:3.23.4")
+    // junit
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
