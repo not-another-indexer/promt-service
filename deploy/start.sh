7b6a9b25ae67a3969ac266b0631e7d7255441db7
DLaaren <dlaaren8@gmail.com>
2024-12-08 06:40:25 +0700

    deploy: add front to deploying scripts

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-#rm -rf cloudberry-storage
-#rm -rf prompt-service
+rm -rf cloudberry-storage
+rm -rf prompt-service
+rm -rf nai-frontend
 
 if [ ! -d "./cloudberry-storage/.git" ]; then
   git clone https://github.com/not-another-indexer/cloudberry-storage.git cloudberry-storage
+fi
+
+if [ ! -d "./nai-frontend/.git" ]; then
+  git clone https://github.com/not-another-indexer/nai-frontend.git nai-frontend
+  cd nai-frontend
+  git clone https://github.com/not-another-indexer/protos.git
+  cd ../
 fi
 
 if [ ! -d "./prompt-service/.git" ]; then
   git clone https://github.com/not-another-indexer/promt-service.git prompt-service
 fi
 
-docker-compose up --build
+sudo docker-compose up --build
